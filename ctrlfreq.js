f9850c429cb23f6501779bc7e3c566065d4fb633
John Aughey <john.h.aughey@boeing.com>
2014-02-12 16:47:46 -0600

    actually works now

--- ctrlfreq.js
+++ ctrlfreq.js
@@ -7,27 +7,51 @@ var crypto = require('crypto');
 var levelup = require('levelup');
 
 var dbpath = "./db/chunks";
-var db = levelup(dbpath);
+
+var db = null; // connected before we process directories.
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var chunkcount = 0;
+var filecount = 0;
+var dircount = 0;
+var bytecount = 0;
+var chunkwrite = 0;
+var bytewrite = 0;
+
+var bytespending = 0;
+var maxbytes = 0;
+
+function bytecheck(len) {
+	bytespending += len;
+	if (bytespending > maxbytes) {
+		//console.log("Bytes in play: " + bytespending);
+		maxbytes = bytespending;
+	}
+	return function() {
+		bytespending -= len;
+	}
+}
 
 function limit(count, dm) {
 	var waiting = [];
-
-	function whendone() {
-		count++;
-		runnext();
-	}
 
 	function runnext() {
 		if (waiting.length == 0) {
 			return;
 		}
 		var next = waiting.shift();
-		count--;
 		var deferred = next[0];
 		var cb = next[1];
 		cb(function(data) {
+			//console.log("Rate limit done: " + dm + " " + waiting.length)
 			count++;
-			runnext();
+			if (waiting.length != 0) {
+				count--;
+				process.nextTick(runnext);
+			}
 			deferred.resolve(data);
 		});
 	}
@@ -38,9 +62,10 @@ function limit(count, dm) {
 				var deferred = Q.defer();
 				waiting.push([deferred, cb]);
 				if (count > 0) {
-					runnext();
+					count--;
+					process.nextTick(runnext);
 				} else {
-					console.log("Rate limiting: " + dm + ":" + debugmessage)
+					//console.log("Rate limiting: " + dm + ":" + debugmessage + " " + waiting.length)
 				}
 				return deferred.promise;
 			}
@@ -48,25 +73,64 @@ function limit(count, dm) {
 	}
 }
 
-function storechunk(buffer) {
-	var deferred = Q.defer();
-	var shasum = crypto.createHash('sha1');
-	shasum.update(buffer);
-	var digest = shasum.digest('hex');
+function noop() {}
 
-	if (true) {
-		db.put(digest, buffer, function(err) {
-			if (err) {
-				console.log("Error writing sha to database: " + err);
-				process.exit(1);
-				deferred.resolve(null);
-				return;
-			}
-			deferred.resolve(digest);
-		});
+function isChunkStored(key, cb) {
+	var iterator = db.db.iterator({
+		start: key,
+		values: false
+	});
+	iterator.next(function(err, ikey) {
+		iterator.end(noop);
+		if (!ikey) {
+			return;
+		}
+		if (ikey == key) {
+			cb(true);
+		} else {
+			cb(false);
+		}
+	})
+}
+
+function storechunk(buffer, precomputedkey, debug) {
+	var deferred = Q.defer();
+
+	if (precomputedkey) {
+		var digest = precomputedkey;
 	} else {
-		deferred.resolve(digest);
+		var shasum = crypto.createHash('sha1');
+		shasum.update(buffer);
+		var digest = shasum.digest('hex');
 	}
+
+	chunkcount++;
+	bytecount += buffer.length;
+
+	//deferred.resolve(digest);
+	//return deferred.promise;
+
+	isChunkStored(digest, function(stored) {
+		if (stored) {
+			deferred.resolve(digest);
+			//console.log("Key: " + digest + " already in database")
+		} else {
+			//console.log("##STORING key " + digest + " " + debug);
+			//console.log(buffer);
+			db.put(digest, buffer, function(err) {
+				//console.log(" ## done storing key " + digest)
+				if (err) {
+					console.log("Error writing sha to database: " + err);
+					process.exit(1);
+					deferred.resolve(null);
+					return;
+				}
+				chunkwrite++;
+				bytewrite += buffer.length;
+				deferred.resolve(digest);
+			});
+		}
+	});
 
 	return deferred.promise;
 }
@@ -77,6 +141,7 @@ var readlimit = limit(100, "readlimit");
 
 function processfile(fullpath) {
 	return filelimit(fullpath).then(function(filedone) {
+		filecount++;
 		fs.open(fullpath, 'r', function(err, fd) {
 			if (err) {
 				filedone(null);
@@ -84,6 +149,7 @@ function processfile(fullpath) {
 			}
 			var chunks = [];
 
+			// This is set to 1 because the close calls checkdone.
 			var outstanding = 1;
 
 			function readnext() {
@@ -93,14 +159,13 @@ function processfile(fullpath) {
 				function checkdone(error) {
 					outstanding -= 1;
 					if (outstanding == 0) {
-						console.log("file done: " + fullpath);
 						filedone(error ? null : chunks);
 					}
 				}
 				//console.log("Trying to read from: " + fullpath)
 				readlimit("Buffer").then(function(bufferdone) {
 					fs.read(fd, buffer, 0, size, null, function(err, bytesread, buffer) {
-						console.log("Reading " + fullpath + " " + bytesread)
+						//console.log("Reading " + fullpath + " " + bytesread)
 						if (err) {
 							console.log("Error reading from " + fullpath);
 							fs.close(fd);
@@ -115,14 +180,16 @@ function processfile(fullpath) {
 							return;
 						}
 						buffer = buffer.slice(0, bytesread);
+						var unlog = bytecheck(buffer.length);
 						var index = chunks.length;
 						chunks.push(null);
 						outstanding += 1;
-						storechunk(buffer).then(function(sha) {
+						storechunk(buffer,null,fullpath).then(function(sha) {
+							unlog();
 							chunks[index] = sha;
 							bufferdone();
 							checkdone();
-						});
+						}).done();
 						readnext();
 					})
 				})
@@ -136,7 +203,10 @@ function processfile(fullpath) {
 
 function processdir(dirname) {
 	console.log("reading dir: " + dirname)
-	return dirlimit(dirname).then(function(dirdone) {
+	// We return our own promise
+	var deferred = Q.defer();
+	dirlimit(dirname).then(function(dirdone) {
+		dircount++;
 		fs.readdir(dirname, function(err, dirfiles) {
 			if (err) {
 				console.log("Error reading " + dirname + ": " + err);
@@ -151,6 +221,7 @@ function processdir(dirname) {
 
 				try {
 					stats.push(Q.nfcall(fs.stat, fullpath).then(function(stat) {
+						var storestat = _.pick(stat,'mode','uid','gid','size','mtime')
 						if (err) {
 							console.log("Could not stat: " + fullpath)
 							return;
@@ -159,16 +230,18 @@ function processdir(dirname) {
 							files.push(processfile(fullpath).then(function(chunks) {
 								return {
 									name: file,
-									stat: stat,
+									stat: storestat,
 									chunks: chunks
 								}
 							}));
 						} else if (stat.isDirectory()) {
+							delete storestat.size;
+							delete storestat.mtime;
 							dirs.push(processdir(fullpath).then(function(dirinfo) {
-								return storechunk(JSON.stringify(dirinfo)).then(function(sha) {
+								return storechunk(JSON.stringify(dirinfo),null,"Directory " + fullpath).then(function(sha) {
 									return {
 										name: file,
-										stat: stat,
+										stat: storestat,
 										sha: sha
 									};
 								});
@@ -178,42 +251,55 @@ function processdir(dirname) {
 				} catch (e) {
 					console.log("Exception on file: " + fullpath);
 					console.log(e);
-
 				}
 			});
-			console.log(dirname + " Waiting on stats: " + stats.length);
+			//console.log(dirname + " Waiting on stats: " + stats.length);
 			Q.all(stats).then(function() {
-				console.log(dirname + " Waiting on files: " + files.length);
+				//console.log(dirname + " Waiting on files: " + files.length);
 				Q.all(files).then(function(files) {
-					console.log(dirname + " Waiting on dirs: " + dirs.length);
+					//console.log(dirname + " Waiting on dirs: " + dirs.length);
+					dirdone();
 					Q.all(dirs).then(function(dirs) {
-						console.log("done with " + dirname)
-						dirdone({
-							dirs: dirs,
-							files: files
-						});
+						deferred.resolve({
+							dirs: _.sortBy(dirs,function(d) { return d.name }),
+							files: _.sortBy(files,function(f) { return f.name })
+						})
 					}).done()
 				}).done()
 			}).done()
 		});
 	})
+	return deferred.promise;
 }
 
 var args = process.argv;
 args.shift()
 args.shift()
-_.each(args, function(dirpath) {
-	processdir(dirpath).then(function(result) {
-		console.log("done processing")
-		console.log(JSON.stringify(result, null, 3));
-		db.put(dirpath, JSON.stringify(result), function() {
-			console.log("Closing db")
-			db.close(function(err) {
-				console.log("Database closed")
-				levelup.repair(dbpath, function() {
-					console.log("Database " + dbpath + " compacted.")
-				})
-			});
+
+Q.nfcall(levelup, dbpath).then(function(d) {
+	db = d; // Setting the global db
+	return Q.all(_.map(args, function(dirpath) {
+		console.log("Kicking off processing: " + dirpath)
+		return processdir(dirpath).then(function(result) {
+			console.log("done processing directory: " + dirpath)
+			var info = JSON.stringify(result, dirpath);
+			return storechunk(info,null,"Top directory " + dirpath);
 		});
-	}).done();
-});
+	}));
+}).then(function() {
+	console.log("Closing db ");
+	return Q.ninvoke(db, 'close');
+}).then(function() {
+	console.log("Database closed")
+	//return Q.ninvoke(levelup, 'repair', dbpath);
+}).then(function() {
+	console.log("Database " + dbpath + " compacted.")
+	console.log("Directory Count: " + numberWithCommas(dircount));
+	console.log("File Count: " + numberWithCommas(filecount));
+	console.log("Chunk Count: " + numberWithCommas(chunkcount));
+	console.log("Byte Count: " + numberWithCommas(bytecount));
+	console.log("Chunks Written: " + numberWithCommas(chunkwrite));
+	console.log("Bytes Written: " + numberWithCommas(bytewrite));
+}).fail(function(err) {
+	console.log("FAILED: " + err);
+}).done();
